fd06c0923f1ac8e0fd6800c08c9e2309257415ba
Hans Adema <hans@xvhost.com>
2016-12-17 20:32:28 +0100

    Remove database based logging and switch to regular Monolog logging

--- config/app.php
+++ config/app.php
@@ -28,6 +28,7 @@ return [
     |
     */
     'locale' => env('APP_LOCALE', 'en'),
+
     /*
     |--------------------------------------------------------------------------
     | Application Fallback Locale
@@ -39,5 +40,21 @@ return [
     |
     */
     'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Logging Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure the log settings for your application. Out of
+    | the box, Laravel uses the Monolog PHP logging library. This gives
+    | you a variety of powerful log handlers / formatters to utilize.
+    |
+    | Available Settings: "single", "daily", "syslog", "errorlog"
+    |
+    */
+    'log' => env('APP_LOG', 'daily'),
+
+    'log_level' => env('APP_LOG_LEVEL', 'notice'),
 
 ];
