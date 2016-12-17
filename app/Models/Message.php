ac3ce94bc986f57458bc80ce92d548711e95dd19
Hans Adema <hans@xvhost.com>
2016-12-17 20:23:58 +0100

    Add migrations and models for address, messages and logs

---
+++ app/Models/Message.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    const STATUS_REJECTED_REMOTE = 'rejected_remote';
+    const STATUS_REJECTED_LOCAL = 'rejected_local';
+    const STATUS_SENT = 'sent';
+
+    const REASON_ADDRESS_BLOCKED = 'address_blocked';
+    const REASON_SPAM_SCORE = 'spam_score';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['from', 'subject', 'status', 'reason', 'spam_score', 'address_id'];
+}
---
+++ database/migrations/2016_12_17_171313_create_logs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('provider');
+            $table->text('request');
+            $table->unsignedInteger('message_id');
+            $table->timestamps();
+            $table->foreign('message_id')->references('id')->on('messages');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('logs');
+    }
+}
